b4d364dd14c25f5430469372f0e73b1c810f44d7
arunkumareee2000 <arunkumareee2000@gmail.com>
2024-07-22 22:59:08 +0530

    while loop by rohit

--- Test1.java
+++ Test1.java
@@ -3,7 +3,12 @@ public class Test1(){
      int a=10;
      for(int i=0;i<=a;i++){
       System.out.println("For loop executed "+i+" times");
+     }
 
+     int b=5;
+     while(b<=10){
+            System.out.println("While loop executed "+a+" times");
+            a++;
      }
 
   }
